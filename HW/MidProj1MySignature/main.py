8250c802f3ad365cc75e72da757d80103bdf9d0f
Haim Adrian <haimadrian@gmail.com>
2021-07-28 00:14:01 +0300

    Add binary demo on small tensor to see the signature

--- HW/MidProj1MySignature/main.py
+++ HW/MidProj1MySignature/main.py
@@ -17,6 +17,30 @@ def readAndResizeImages(*imageNames):
     return result
 
 
+def testOnSmallTensor():
+    # 00000001, 00000010, 00000011
+    # 00000100, 00010111, 00000110
+    # 00000111, 00001111, 11111111
+    imageTest = np.array([[1, 2,  3],
+                          [4, 23, 6],
+                          [7, 15, 255]], dtype=np.uint8)
+
+    # 11100000, 10100000, 00000011
+    # 11100000, 10100000, 00000011
+    # 11100000, 10100000, 00000011
+    signatureTest = np.array([[224, 160, 3],
+                              [224, 160, 3],
+                              [224, 160, 3]], dtype=np.uint8)
+
+    signedImage = my_sign(imageTest, signatureTest)
+    print('Image:')
+    print(np.vectorize(np.binary_repr)(imageTest, width=8))
+    print('Signature:')
+    print(np.vectorize(np.binary_repr)(signatureTest, width=8))
+    print('Signed Image:')
+    print(np.vectorize(np.binary_repr)(signedImage, width=8))
+
+
 if __name__ == '__main__':
     # Read and resize images
     signature, gotenksImage = readAndResizeImages('guitar.jpg', 'gotenks.jpg')
@@ -33,4 +57,6 @@ if __name__ == '__main__':
     showImageUsingMatPlotLib('My Signature', True, (gotenksImage, 221, 'Original'), (signature, 222, 'Signature'),
                              (gotenksImageSigned, 223, 'Signed Image'), (gotenksFake, 224, 'Is valid signature? (' +
                                                                          str(isImageSignatureValid) + ')'))
+else:
+    testOnSmallTensor()
 
